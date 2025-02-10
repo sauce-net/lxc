86a382597db6c6017cdfb8baba1ebf09cbd089aa
codyconfer <mail@codyconfer.me>
2025-02-09 17:36:42 -0700

    poetry

--- alp/poetry-alp-lxc.sh
+++ alp/poetry-alp-lxc.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
-apt update
-apt install pipx
-pipx ensurepath
-pipx install poetry
+apk add --update --no-cache python3 && ln -sf python3 /usr/bin/python
+python3 -m ensurepip
+pip3 install --no-cache --upgrade pip setuptools
+apk add --no-cache \
+  curl \
+  gcc \
+  libressl-dev \
+  musl-dev \
+  libffi-dev \
+&& curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --profile=minimal \ 
+&& source $HOME/.cargo/env \ 
+&& pip install --no-cache-dir poetry \
+&& apk del \
+  curl \
+  gcc \
+  libressl-dev \
+  musl-dev \
+  libffi-dev
